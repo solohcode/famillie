47cdcfe8caed9b8bb4fd636440a4a6bbfdd88984
unknown <tarrmmy@gmail.com>
2023-02-23 15:35:52 +0100

    first commit

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './style.css'
 
 function FaqPage() {
   return (
-    <div>FaqPage</div>
+    <div >
+      <div 
+        className='faq px-4 text-center'
+      >
+        <h1 className='my-5 '>Frequently Asked Questions</h1>
+        <div>
+          <h5>We have answers to your Question</h5>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
