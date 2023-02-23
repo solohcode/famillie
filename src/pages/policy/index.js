a58942e420dfb0036e1fb4609003121ec29e02a2
Rofiat <rofiah.olaniyi@gmail.com>
2023-02-23 12:45:05 +0100

    adjustments

---
+++ src/pages/policy/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/header'
+
+
+const Policy = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default Policy
