81fd863323a0dcc49a852a4e45373ee61f38e152
unknown <tarrmmy@gmail.com>
2023-02-23 19:59:18 +0100

    accordion

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,18 +1,40 @@
 import React from 'react'
+import { TextInput } from 'flowbite-react'
+import {CiSearch} from 'react-icons/ci'
 import './style.css'
+import Accorddion from './accordion'
+import Accorddionn from './secondaccordion'
 
 function FaqPage() {
   return (
-    <div >
       <div 
-        className='faq px-4 text-center'
+        className='faq px-4 '
       >
-        <h1 className='my-5 '>Frequently Asked Questions</h1>
-        <div>
-          <h5>We have answers to your Question</h5>
-          
+        <h1 className='my-5 text-center text-3xl'>Frequently Asked Questions</h1>
+        <div className='flex'>
+          <h6 className='text-xl text-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+            <span>We have answers to </span>
+            <span>your Question</span>
+          </h6>
+          <div className='w-3/4'>
+            <TextInput
+              className='px-5 py-5 bg-transparent border-slate-400 '
+              id="email4"
+              type="text"
+              icon={CiSearch}
+              placeholder="Search a Question"
+              required={true}
+            />
+          </div>
         </div>
-      </div>
+        <div className='flex my-7'>
+          <p className='mx-7 font-bold'>General Question</p>
+          <Accorddion />
+        </div>
+        <div className='flex my-7'>
+          <p  className='mx-7 font-bold'>Safety & Security</p>
+          <Accorddionn />
+        </div>
     </div>
   )
 }
---
+++ src/pages/faq/secondaccordion/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Accordion } from 'flowbite-react'
+import {AiOutlinePlusCircle} from 'react-icons/ai'
+
+
+const Accorddionn = () => {
+  return (
+    <div>
+        <Accordion arrowIcon={AiOutlinePlusCircle}>
+            <Accordion.Panel>
+                <Accordion.Title>
+                Is my information safe with 4Famillie?
+                </Accordion.Title>
+                <Accordion.Content>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen.
+                </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>
+                Do 4Famillie App have access to my Wallet?
+                </Accordion.Title>
+                <Accordion.Content>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen.
+                </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            <Accordion.Panel>
+                <Accordion.Title>
+                Do 4Famillie App have access to my Wallet?
+                </Accordion.Title>
+                <Accordion.Content>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen. Screen time feature to help Parents track kids time on screen.
+                </p>
+                </Accordion.Content>
+            </Accordion.Panel>
+            </Accordion>
+    </div>
+  )
+}
+
+export default Accorddionn
