1707310430eaa8bbed2a794235cfdad184b28a2e
unknown <tarrmmy@gmail.com>
2023-02-25 14:57:04 +0100

    new accordions updated

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -7,14 +7,14 @@ import Testing from './firstaccordion'
 
 function FaqPage() {
   return (
-      <div className='faq px-4 container'>
+      <div className='faq px-7'>
         <h1 className='my-5 font-bold text-center text-3xl'>Frequently Asked Questions</h1>
         <div className='flex items-center'>
           <span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-accent md:from-primary to-primary md:to-accent'> We have answers to your Question </span>
           <div className='w-full'>
             <TextInput
               className='px-5 py-7 bg-white border-slate-400 '
-              id="email4"
+              id="email"
               type="text"
               icon={CiSearch}
               placeholder="Search a Question"
@@ -22,11 +22,11 @@ function FaqPage() {
             />
           </div>
         </div>
-        <div className='flex my-7'>
+        <div className='flex flex-row px-4'>
           <p className='mx-7 font-bold'>General Question</p>
           <Testing />
         </div>
-        <div className='flex  my-7'>
+        <div className='flex flex-row px-4'>
           <p  className='mx-7 font-bold'>Safety & Security</p>
           <Accorddionn />
         </div>
